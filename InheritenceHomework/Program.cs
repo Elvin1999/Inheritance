8e59091dc9aca699f4f68cac543f5289a955e378
camalzade_elvin <camalzade_elvin@outlook.com>
2018-11-14 13:49:01 +0400

    Begninning of project

---
+++ InheritenceHomework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceHomework
+{
+    class Document
+    {
+
+    }
+    class ProDocument : Document { }
+    class ExpertDocument : ProDocument { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
